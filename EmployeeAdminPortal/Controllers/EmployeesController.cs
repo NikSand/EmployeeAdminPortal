96a94c0c09fb252cb5df20f3f6fece83a83107a2
Никита <sandalov-1997@mail.ru>
2024-07-13 13:55:38 +0300

    Добавьте файлы проекта.

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+
+    }
+}
---
+++ EmployeeAdminPortal/Data/AplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmployeeAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeAdminPortal.Data
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base (options) 
+        {
+            // ctor - create constructor
+            //prop - create properties
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
